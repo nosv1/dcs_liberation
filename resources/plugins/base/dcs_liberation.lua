a6cd6f309688ada9b4927ddf218b89aa56bc6abb
Chris Seagraves <christopher.seagraves@gmail.com>
2022-12-29 23:00:17 -0600

    Tracking damaged objects, possible to use this for knowing if object died on accident or by combat

--- resources/plugins/base/dcs_liberation.lua
+++ resources/plugins/base/dcs_liberation.lua
@@ -4,6 +4,7 @@ local WRITESTATE_SCHEDULE_IN_SECONDS = 60
 logger = mist.Logger:new("DCSLiberation", "info")
 logger:info("Check that json.lua is loaded : json = "..tostring(json))
 
+damaged_objects = {} -- damaged units will be added via S_EVENT_HIT event
 killed_aircrafts = {} -- killed aircraft will be added via S_EVENT_CRASH event
 killed_ground_units = {} -- killed units will be added via S_EVENT_DEAD event
 base_capture_events = {}
@@ -30,6 +31,7 @@ function write_state()
 
     local fp = io.open(_debriefing_file_location, 'w')
     local game_state = {
+        ["damaged_aircrafts"] = damaged_aircrafts,
         ["killed_aircrafts"] = killed_aircrafts,
         ["killed_ground_units"] = killed_ground_units,
         ["base_capture_events"] = base_capture_events,
@@ -142,10 +144,15 @@ end
 
 activeWeapons = {}
 local function onEvent(event)
-   if event.id == world.event.S_EVENT_CRASH and event.initiator then
-       killed_aircrafts[#killed_aircrafts + 1] = event.initiator.getName(event.initiator)
-       write_state()
-   end
+    if event.id == world.event.S_EVENT_HIT and event.target then
+        damaged_objects[#damaged_objects + 1] = event.target.getName(event.target)
+        write_state()
+    end
+
+    if event.id == world.event.S_EVENT_CRASH and event.initiator then
+        killed_aircrafts[#killed_aircrafts + 1] = event.initiator.getName(event.initiator)
+        write_state()
+    end
 
     if event.id == world.event.S_EVENT_DEAD and event.initiator then
         killed_ground_units[#killed_ground_units + 1] = event.initiator.getName(event.initiator)
