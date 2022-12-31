a879b0a641f63a00c7988da1161a1b2163b078cf
Chris <christopher.seagraves@gmail.com>
2022-12-30 18:27:54 -0600

    strike flights now have logic to spread out their ordance if there are flights ahead attacking the same wayponit with multiple targets
    ideally using only one bomb per target over the whole sortie

--- game/missiongenerator/aircraft/waypoints/strikeingress.py
+++ game/missiongenerator/aircraft/waypoints/strikeingress.py
@@ -4,6 +4,9 @@ from dcs import Point
 from dcs.planes import B_17G, B_52H, Tu_22M3
 from dcs.point import MovingPoint
 from dcs.task import Bombing, Expend, OptFormation, WeaponType
+from game.ato.flightplans.strike import StrikeFlightPlan
+from game.ato.flightwaypoint import FlightWaypoint
+from game.theater.theatergroup import TheaterUnit
 
 from .pydcswaypointbuilder import PydcsWaypointBuilder
 
@@ -32,7 +35,51 @@ class StrikeIngressBuilder(PydcsWaypointBuilder):
         waypoint.tasks.append(bombing)
 
     def add_strike_tasks(self, waypoint: MovingPoint) -> None:
-        for target in self.waypoint.targets:
+        # The code below shifts the targets for the strike waypoint to prevent later strike flights from attacking the same targets.
+        strike_flights_ahead: int = 0
+        total_strike_flights: int = 0
+        targets = [t for t in self.waypoint.targets]
+
+        if isinstance(self.flight.flight_plan, StrikeFlightPlan):
+            for flight in self.package.flights:
+                if not isinstance(flight.flight_plan, StrikeFlightPlan):
+                    continue
+                # found a flight that is a strike flight
+
+                for fp_waypoint in flight.flight_plan.waypoints:
+                    if not len(fp_waypoint.targets) > 0:
+                        continue
+                    # found waypoint with targets
+
+                    if not (
+                        [(t.x, t.y) for t in fp_waypoint.targets]
+                        == [(t.x, t.y) for t in self.waypoint.targets]
+                    ):
+                        continue
+                    # found waypoint with same targets
+
+                    total_strike_flights += 1
+                    if flight == self.flight:
+                        break  # this assumes the flights are in order
+                    strike_flights_ahead += 1
+
+            # shift amount example:
+            # 2 total strike flights, 1 strike flight ahead, 8 targets
+            # shift amount = targets / total strike flights = 8 / 2 = 4
+            # start index = strike flights ahead * shift amount = 1 * 4 = 4
+            # we wrap to the beginning of the list if we go past the end
+            shift_amount: int = int(len(targets) / total_strike_flights)
+            if strike_flights_ahead > 0:
+                i: int = strike_flights_ahead * shift_amount
+                new_targets: list[TheaterUnit] = []
+                while len(new_targets) < len(targets):
+                    new_targets.append(targets[i])
+                    i += 1
+                    if i >= len(targets):
+                        i = 0
+                targets = new_targets
+
+        for target in targets:
             bombing = Bombing(
                 target.position, weapon_type=WeaponType.Auto, group_attack=True
             )
